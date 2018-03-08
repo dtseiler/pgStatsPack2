daddff4eedf2982eabf5f0c2401996b5ed480540
Don Seiler <don@seiler.us>
2018-03-08 11:25:55 -0600

    Fixing names

--- sql/pgstatspack2_delete_snap.sql
+++ sql/pgstatspack2_delete_snap.sql
@@ -1,5 +1,5 @@
 SET client_min_messages TO error;
--- Create pgstatspack_snap procedure.
+-- Create pgstatspack2_delete_snap procedure.
 --
 -- By frits.hoogland@interaccess.nl
 -- Based on Glenn.Fawcett@Sun.com's snap procedure
@@ -15,21 +15,21 @@ DECLARE
 BEGIN
     SELECT current_timestamp - interval '30 days' INTO old_snap_time;
 
-    SELECT max(snapid) INTO old_snap_id FROM pgstatspack_snap WHERE ts < old_snap_time;
+    SELECT max(snapid) INTO old_snap_id FROM pgstatspack2.pgstatspack2_snap WHERE ts < old_snap_time;
 
     SELECT 'Deleted '||count(snapid)||' snapshots older than '||old_snap_time
     INTO message 
-    FROM pgstatspack_snap 
+    FROM pgstatspack2.pgstatspack2_snap 
     WHERE snapid <= old_snap_id;
 
-    DELETE FROM pgstatspack_snap WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_database WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_tables WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_indexes WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_sequences WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_settings WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_statements WHERE snapid <= old_snap_id;
-    DELETE FROM pgstatspack_bgwriter WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_snap WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_database WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_tables WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_indexes WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_sequences WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_settings WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_statements WHERE snapid <= old_snap_id;
+    DELETE FROM pgstatspack2.pgstatspack2_bgwriter WHERE snapid <= old_snap_id;
 
     RETURN message;
 END;

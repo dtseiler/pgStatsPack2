9ec4b7945409630c49aed38f647926e7a426790f
Don Seiler <don@seiler.us>
2018-03-08 10:28:22 -0600

    Tune up to get install working

--- install_pgStatsPack2.sh
+++ install_pgStatsPack2.sh
@@ -6,15 +6,15 @@ PSQL="psql -qX --set ON_ERROR_STOP=on"
 
 install_stats () {
         set -e
-        $PSQL -d "${dbname}" -f "sql/pgstatspack_create_schema.sql"
-        $PSQL -d "${dbname}" -f "sql/pgstatspack_create_tables.sql"
-        $PSQL -d "${dbname}" -f "sql/pgstatspack_create_snap.sql"
-        $PSQL -d "${dbname}" -f "sql/pgstatspack_delete_old_stats.sql"
-        $PSQL -d "${dbname}" -f "sql/pgstatspack_get_unused_indexes.sql"
+        $PSQL -d "${dbname}" -f "sql/pgstatspack2_create_schema.sql"
+        $PSQL -d "${dbname}" -f "sql/pgstatspack2_create_tables.sql"
+        $PSQL -d "${dbname}" -f "sql/pgstatspack2_create_snap.sql"
+        $PSQL -d "${dbname}" -f "sql/pgstatspack2_delete_old_stats.sql"
+        $PSQL -d "${dbname}" -f "sql/pgstatspack2_get_unused_indexes.sql"
         set +e
 }
 
-for dbname in $($PSQL -t -f "sql/get_db_names.sql")
+for dbname in $($PSQL -d postgres -t -f "sql/get_db_names.sql")
 do
         echo "Results for database ${dbname}"
         if [ $($PSQL -d "${dbname}" -t -c "select count(lanname) from pg_language where lanname='plpgsql';") -lt 1 ]

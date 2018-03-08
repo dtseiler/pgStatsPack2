a7ab16edea01c54b68294de9b621a74103c8cee8
Don Seiler <don@seiler.us>
2018-03-08 11:51:08 -0600

    Re-formatted and name-corrected get_unused_indexes

--- sql/pgstatspack2_get_unused_indexes.sql
+++ sql/pgstatspack2_get_unused_indexes.sql
@@ -1,67 +1,67 @@
 SET client_min_messages TO error;
--- Function: get_unused_indexes(interval)
+-- Function: pgstatspack2_get_unused_indexes(interval)
 
 SET search_path TO pgstatspack2,public;
 
-DROP FUNCTION if exists get_unused_indexes(interval);
+DROP FUNCTION if exists pgstatspack2_get_unused_indexes(interval);
 
-CREATE OR REPLACE FUNCTION get_unused_indexes(IN p_timespan interval)
-  RETURNS TABLE(table_name text, index_name text, size text, indexdef text) AS
+CREATE OR REPLACE FUNCTION pgstatspack2_get_unused_indexes(IN p_timespan interval)
+    RETURNS TABLE(table_name text, index_name text, size text, indexdef text) AS
 $BODY$
 DECLARE
- l_max_ts timestamp;
- l_snapid_start integer;
- l_snapid_stop integer;
- l_timespan interval := '1 week'; -- default
+    l_max_ts timestamp;
+    l_snapid_start integer;
+    l_snapid_stop integer;
+    l_timespan interval := '1 week'; -- default
 BEGIN
- -- this function returns the name of the unused indexes and their corresponding table name.
- -- author: ubartels
- -- date: 26/10/2010
- --
- -- prerequsites: pgstatspack
+    -- this function returns the name of the unused indexes and their corresponding table name.
+    -- author: ubartels
+    -- date: 26/10/2010
+    --
+    -- prerequsites: pgstatspack2
  
- if p_timespan is not null then
-  l_timespan = p_timespan;
- end if;
+    IF p_timespan IS NOT NULL THEN
+        l_timespan = p_timespan;
+    END IF;
  
- l_max_ts := max(ts) from pgstatspack_snap;
- l_snapid_stop  := snapid from pgstatspack_snap where ts=l_max_ts;
- l_snapid_start := snapid from pgstatspack_snap where ts > l_max_ts - l_timespan order by ts asc limit 1;
+    l_max_ts := MAX(ts) FROM pgstatspack2.pgstatspack2_snap;
+    l_snapid_stop  := snapid FROM pgstatspack2.pgstatspack2_snap WHERE ts=l_max_ts;
+    l_snapid_start := snapid FROM pgstatspack2.pgstatspack2_snap WHERE ts > l_max_ts - l_timespan ORDER BY ts ASC LIMIT 1;
 
- -- check if there is any data
- if l_snapid_start is null or l_snapid_stop is null then
-  raise info 'no data found for the timespan of %.',l_timespan;
-  return;
- end if;
+    -- check if there is any data
+    IF l_snapid_start IS NULL OR l_snapid_stop IS NULL THEN
+        raise info 'no data found for the timespan of %.',l_timespan;
+        RETURN;
+    END IF;
 
- -- check if the stats are active or stale
- if l_max_ts < now()-'1 day'::interval then
-  raise info 'pgstatspack data is stale (older than 1 day). please get it up and running first.';
-  return;
- end if;
+    -- check if the stats are active or stale
+    IF l_max_ts < now()-'1 day'::interval THEN
+        raise info 'pgstatspack data is stale (older than 1 day). please get it up and running first.';
+        RETURN;
+    END IF;
 
- return query
- select a.table_name::text, a.index_name::text ,pg_size_pretty(pg_relation_size(c.oid))::text, pg_get_indexdef(c.oid)::text
- from pg_class c, pg_namespace n, pg_index i, pgstatspack_indexes a, pgstatspack_indexes b
-where
- a.snapid=l_snapid_start and 
- b.snapid=l_snapid_stop and
- a.index_name=b.index_name and
- a.table_name=b.table_name and
- (b.idx_scan-a.idx_scan) = 0 and
- (b.idx_tup_read-a.idx_tup_read) = 0 and
- (b.idx_tup_fetch-a.idx_tup_fetch) = 0 and
- a.index_name=n.nspname||'.'||c.relname and
- n.oid=c.relnamespace and
- c.oid=i.indexrelid and
- i.indisprimary is false and
- i.indisunique is false and
- i.indisclustered is false and
- a.index_name not in (select n.nspname||'.'||conname from pg_constraint where contype='f')
-order by pg_relation_size(c.oid) desc;
+    RETURN query
+    SELECT a.table_name::text, a.index_name::text ,pg_size_pretty(pg_relation_size(c.oid))::text, pg_get_indexdef(c.oid)::text
+    FROM pg_class c, pg_namespace n, pg_index i, pgstatspack2.pgstatspack2_indexes a, pgstatspack2.pgstatspack2_indexes b
+    WHERE
+        a.snapid=l_snapid_start AND 
+        b.snapid=l_snapid_stop AND
+        a.index_name=b.index_name AND
+        a.table_name=b.table_name AND
+        (b.idx_scan-a.idx_scan) = 0 AND
+        (b.idx_tup_read-a.idx_tup_read) = 0 AND
+        (b.idx_tup_fetch-a.idx_tup_fetch) = 0 AND
+        a.index_name=n.nspname||'.'||c.relname AND
+        n.oid=c.relnamespace AND
+        c.oid=i.indexrelid AND
+        i.indisprimary IS FALSE AND
+        i.indisunique IS FALSE AND
+        i.indisclustered IS FALSE AND
+        a.index_name not in (SELECT n.nspname||'.'||conname FROM pg_constraint WHERE contype='f')
+    ORDER BY pg_relation_size(c.oid) DESC;
 
 END;
 $BODY$
-  LANGUAGE 'plpgsql' VOLATILE SECURITY DEFINER
-  COST 100
-  ROWS 1000;
+    LANGUAGE 'plpgsql' VOLATILE SECURITY DEFINER
+    COST 100
+    ROWS 1000;

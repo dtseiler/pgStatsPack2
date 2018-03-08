e0f55e3b0352650b251f310aa12c261c33d1b496
Don Seiler <don@seiler.us>
2018-03-08 11:19:13 -0600

    Use the new names

--- sql/pgstatspack2_exists.sql
+++ sql/pgstatspack2_exists.sql
@@ -2,12 +2,12 @@ SELECT COUNT(tablename)
 FROM pg_tables
 WHERE schemaname='pgstatspack2'
 AND tablename IN (
-      'pgstatspack_snap'
-    , 'pgstatspack_database'
-    , 'pgstatspack_tables'
-    , 'pgstatspack_indexes'
-    , 'pgstatspack_sequences'
-    , 'pgstatspack_settings'
-    , 'pgstatspack_version'
-    , 'pgstatspack_statements'
+      'pgstatspack2_snap'
+    , 'pgstatspack2_database'
+    , 'pgstatspack2_tables'
+    , 'pgstatspack2_indexes'
+    , 'pgstatspack2_sequences'
+    , 'pgstatspack2_settings'
+    , 'pgstatspack2_version'
+    , 'pgstatspack2_statements'
 );
